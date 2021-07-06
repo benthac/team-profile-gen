acb1c1e3a89486bd058a88e2e2397dab3f682b4f
ben thacker <benthac@gmail.com>
2021-07-05 22:44:57 -0400

    new functionality

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { generateHTML } = require('./generateHTML');
+const generateHTML  = require('./generateHTML');
+const Intern = require('./lib/intern');
+const Engineer = require('./lib/engineer');
+const Manager = require('./lib/manager');
+const teamArry = [];
 
 const questions = [
     {
@@ -31,20 +35,38 @@ const questions = [
         message: 'What is your job title',
         choices: ['manager', 'intern', 'engineer']
 
-    }
-];
+    },
+]
+.then(function({ name, id, email, job}){
+    newPrompt(name, id, email, job)
+})
 
-function writeToFile(answers) {
-    fs.writeFile('index.HTML', generateHTML(answers), function(err) {
-        if (err) {
-          return console.log(err);
-        }; 
+// function writeToFile(answers) {
+//     fs.writeFile('index.HTML', generateHTML(answers), function(err) {
+//         if (err) {
+//           return console.log(err);
+//         }; 
+//     });
+// }
+
+function newPrompt(name, id, email, job) {
+    if (job === 'manager') {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'officenumber',
+                message: 'What is your office number'
+            }
+        ])
+    }
+
+}
 
 function init() {
     inquirer.prompt(questions)
-    .then(answers => {
-    return writeToFile(answers);
-    })
+        // .then(answers => {
+    // return writeToFile(answers);
+    // })
 }
 
 init();
